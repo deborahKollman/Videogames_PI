f0daa0e6aa5c715f0a4c4a96bc3670a57a92bf2b
Debo <deborahkollman@gmail.com>
2022-05-07 11:39:42 -0300

    BD agregada

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,31 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      primaryKey:true
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
+    description:{
+      type:DataTypes.TEXT,
+      allowNull:false
+    },
+    released:{
+      type:DataTypes.DATEONLY
+    },
+    rating:{
+      type:DataTypes.FLOAT
+    },
+    platforms:{
+      type:DataTypes.ARRAY,
+      allowNull:false,
+      defaultValue:[]
+    }
+  },{
+    timestamps:false
   });
 };
